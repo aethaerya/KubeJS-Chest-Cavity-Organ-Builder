92255c6e71f0190a23c60ea83dffdaadf44feaca
TheOnlyMann <82017462+TheOnlyMann@users.noreply.github.com>
2022-07-06 23:56:37 +0900

    publish

--- server_scripts/organregistry.js
+++ server_scripts/organregistry.js
@@ -1,13 +1,19 @@
 //Builder
 function Organ(itemID) {
-    this.itemID = itemID;
-    this.pseudoOrgan = false;
-    this.organScores = [];
+    this.itemID = itemID
+    this.pseudoOrgan = false
+    this.organScores = []
 }
 
 Organ.prototype = {
     addScore: function(score, value) {
-        this.organScores.push({'id': `chestcavity:${score}`, 'value': value});
+        if(score.indexOf('\:') != -1){
+            this.organScores.push({'id': score, 'value': value});
+        }
+        else
+        {
+            this.organScores.push({'id': `chestcavity:${score}`, 'value': value});
+        }
         return this;
     },
 
@@ -24,15 +30,19 @@ Organ.prototype = {
 //Main
 onEvent('server.datapack.high_priority', event => {
     function registerOrgan(organ) {
-        const modid = organ.itemID.split('\:')[0]
-        const item = organ.itemID.split('\:')[1]
-        event.addJson(`chestcavity:organs/kubejs/${modid}/${item}.json`, organ)
+        if(organ.itemID.indexOf('\:') == -1){
+            organ.itemID = `kubejscavity:${organ.itemID}`
+        }
+        modid = organ.itemID.split('\:')[0]
+        item = organ.itemID.split('\:')[1]
+        event.addJson(`chestcavity:organs/kubejs/kubejscavity/${item}.json`, organ)
     }
 
     //Register your organs here!
     //Refer to https://github.com/Tigereye504/chestcavity/blob/master/src/main/java/net/tigereye/chestcavity/registration/CCOrganScores.java
     //for valid Organ Scores
 
+    /*
     registerOrgan(new Organ('minecraft:beetroot').addScore('health', 10).addScore('speed', 10).build()) //example.
     registerOrgan(new Organ('kubejs:someorgan').addScore('health', 2).addScore('speed', 20).pseudo().build()) //pseudo organ example for kubejs item
 
@@ -42,4 +52,42 @@ onEvent('server.datapack.high_priority', event => {
         .addScore('speed', 10)
         .build()
     )
+    */
+    registerOrgan(new Organ('kubejscavity:creative_soul')
+        .addScore('filtration',2)
+        .addScore('breath_recovery',2)
+        .addScore('nutrition',4)
+        .addScore('nerves',1)
+        .addScore('strength',8)
+        .addScore('health',1)
+        .addScore('breath_capacity',2)
+        .addScore('detoxification',1)
+        .addScore('speed',8)
+        .addScore('endurance',2)
+        .addScore('luck',1)
+        .addScore('defense',4.5)
+        .addScore('digestion',1)
+        .addScore('metabolism',1)
+        .build()
+    )
+    registerOrgan(new Organ('actuator').addScore('speed',1.5).addScore('strength',0.5).build())
+    registerOrgan(new Organ('blood_pump').addScore('health',0.5).addScore('metabolism',0.25).addScore('detoxification',0.25).build())
+    registerOrgan(new Organ('captured_soul').addScore('buoyant',3.25).build())
+    registerOrgan(new Organ('clockwork_core').addScore('health',0.5).addScore('defense',0.5).addScore('speed',0.5).addScore('strength',0.5).addScore('endurance',-1).build())
+    registerOrgan(new Organ('copper_frame').addScore('defense',0.5).addScore('knockback_resistant',1).build())
+    registerOrgan(new Organ('copper_wiring').addScore('nerves',1.25).addScore('defense',0.25).build())
+    registerOrgan(new Organ('filter_pump').addScore('detoxification',0.5).addScore('filtration',0.75).build())
+    registerOrgan(new Organ('gem_holster').addScore('luck',1.25).addScore('buoyant',-0.25).addScore('speed',-0.25).build())
+    registerOrgan(new Organ('golden_wiring').addScore('nerves',2).build())
+    registerOrgan(new Organ('hasty_muscle').addScore('speed',1.25).addScore('strength',0.25).addScore('nerves',0.5).build())
+    registerOrgan(new Organ('heart_of_gold').addScore('health',0.5).addScore('luck',0.5).addScore('defense',0.5).addScore('strength',-1).build())
+    registerOrgan(new Organ('heart_of_gold').addScore('health',0.5).addScore('luck',0.5).addScore('defense',0.5).addScore('strength',-1).build())
+    registerOrgan(new Organ('iron_lung').addScore('breath_recovery',0.75).addScore('breath_capacity',0.75).addScore('endurance',1.25).build())
+    registerOrgan(new Organ('jump_shaft').addScore('speed',1.25).addScore('strength',0.25).addScore('impact_resistant',0.75).addScore('leaping',0.5).build())
+    registerOrgan(new Organ('phantom_pump').addScore('buoyant',0.5).addScore('breath_recovery',0.75).addScore('breath_capacity',0.75).addScore('endurance',0.75).build())
+    registerOrgan(new Organ('steel_actuator').addScore('speed',0.25).addScore('strength',1.25).addScore('defense',0.25).build())
+    registerOrgan(new Organ('steel_cable').addScore('nerves',1.25).addScore('defense',0.75).addScore('metabolism',-1).build())
+    registerOrgan(new Organ('steel_core').addScore('health',1).addScore('defense',1).addScore('metabolism',-1).build())
+    registerOrgan(new Organ('steel_detoxifier').addScore('detoxification',0.75).addScore('filtration',0.5).addScore('endurance',0.5).addScore('metabolism',-0.5).build())
+    registerOrgan(new Organ('steel_frame').addScore('defense',1.25).addScore('metabolism',-0.25).build())
 })
---
+++ startup_scripts/organgeneration.js
@@ -0,0 +1,41 @@
+onEvent('item.registry', event => {
+    function createOrgan(organID,StackSize){
+        //StackSize ?? 1
+        if(organID.indexOf('\:') != -1)
+        {   
+            event.create(organID).maxStackSize(StackSize == null ? 1 : StackSize).group('chestcavity:organs')
+        }
+        else
+        {
+            event.create(`kubejscavity:${organID}`).maxStackSize(StackSize == null ? 1 : StackSize).group('chestcavity:organs')
+        }
+    }
+    //Create kubejs Organs here
+    //enter Id of the item and stack size of the item to get a custom item nested inside kubejscavity namespace. 
+    //This DOES NOT make these items into organs automatically, so go to server_scripts to add them to be organs!
+    
+    //thank you for A5TR0spud for allowing me to use his mod's spritework for these examplary organs! 
+    //check out his mod and its github: https://www.curseforge.com/minecraft/mc-mods/ccastroadds https://github.com/A5TR0spud/chest-cavity-astro-adds
+    //createOrgan('clockwork_heart',1)
+    createOrgan('creative_soul')
+    createOrgan('actuator',16)
+    createOrgan('copper_wiring',4)
+    createOrgan('copper_frame',4)
+    createOrgan('phantom_pump')
+    createOrgan('filter_pump')
+    createOrgan('blood_pump')
+    createOrgan('jump_shaft',16)
+    createOrgan('golden_wiring',4)
+    createOrgan('clockwork_core')
+    createOrgan('gem_holster')
+    createOrgan('acid_mixer')
+    createOrgan('steel_actuator',16)
+    createOrgan('steel_cable',4)
+    createOrgan('steel_frame',4)
+    createOrgan('steel_detoxifier')
+    createOrgan('steel_core')
+    createOrgan('captured_soul')
+    createOrgan('iron_lung')
+    createOrgan('heart_of_gold')
+    createOrgan('hasty_muscle',16)
+})
