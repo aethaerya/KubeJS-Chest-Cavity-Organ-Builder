e20dc2b3c719a3122a4fc38b59fadacfbb7e09fb
TheOnlyMann <82017462+TheOnlyMann@users.noreply.github.com>
2022-07-09 15:11:20 +0900

    update of descriptions

--- startup_scripts/organgeneration.js
+++ startup_scripts/organgeneration.js
@@ -11,8 +11,10 @@ onEvent('item.registry', event => {
         }
     }
     //Create kubejs Organs here
-    //enter Id of the item and stack size of the item to get a custom item nested inside kubejscavity namespace. 
+    //Enter ID of the item and stacksize(optional, if not written it will default to 1) of the item to get a custom item nested inside kubejscavity namespace. 
+    //Enter Full ID of the item and stacksize(optional, if not written it will default to 1) of the item to get a custom item nested inside the appropriate namespace.
     //This DOES NOT make these items into organs automatically, so go to server_scripts to add them to be organs!
+    //You also need to put the item textures under the appropriate namespace, defaulted into assets/kubejscavity/textures/item. change kubejscavity with the appropriate namespace.
     
     //thank you for A5TR0spud for allowing me to use his mod's spritework for these examplary organs! 
     //check out his mod and its github: https://www.curseforge.com/minecraft/mc-mods/ccastroadds https://github.com/A5TR0spud/chest-cavity-astro-adds
