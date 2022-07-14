f756800d7749caf8f610c833b5482e78257c07fa
aethaerya <aethaerya@gmail.com>
2022-07-15 05:04:49 +0800

    Major Rework to startup script organgeneration.js
    Adjusted primary function to work with a prototype, where the prototype handles the default values.
    Changed nested if/else statements to ternary operations and removed the double negative checks (.indexOf() != -1 changed to .indexOf() == -1 and swapped cases accordingly)
    Adjusted examples accordingly to reflect the new method of registration, added some more concise comments (previously, certain crucial details were left out)

--- startup_scripts/organgeneration.js
+++ startup_scripts/organgeneration.js
@@ -1,51 +1,107 @@
-onEvent('item.registry', event => {
-    function createOrgan(organID,StackSize,organName){
-        //StackSize ?? 1
-        if(StackSite == null) 
-            StackSize = 1
-        if(organName == null)
-        {
-            if(organID.indexOf('\:') != -1)
-                event.create(organID).maxStackSize(StackSize).group('chestcavity:organs')
-            else
-                event.create(`kubejscavity:${organID}`).maxStackSize(StackSize).group('chestcavity:organs')
-        }
-        else
-        {
-            if(organID.indexOf('\:') != -1)
-                event.create(organID).maxStackSize(StackSize).group('chestcavity:organs').displayName(organName)
-            else
-                event.create(`kubejscavity:${organID}`).maxStackSize(StackSize).group('chestcavity:organs').displayName(organName)
-        }
+function KubeOrgan(organID) {
+    this.itemID = organID.indexOf(':') == -1 ? `kubejscavity:${organID}` : organID // Sets default itemID
+    // If an modID separated by a ':' is not found in string organID, default to kubejscavity, else, keep organID
+    this.stackSize = 1
+    this.displayName = organID.indexOf(':') == -1 ? organID : organID.split(':')[1] // Sets default displayName
+    // If a modid separated by a ':' is not found in string organID, default to organID, else, get string after ':'
+}
+
+KubeOrgan.prototype() = {
+    stackSize: function(size) {
+        this.stackSize = size
+        return this
+    },
+
+    displayName: function(name) {
+        this.displayName()
+        return this
+    },
+
+    build: function() {
+        return this
     }
-    //Create kubejs Organs here
-    //Enter ID of the item and stacksize(optional, if not written it will default to 1) of the item to get a custom item nested inside kubejscavity namespace. 
-    //Enter Full ID of the item and stacksize(optional, if not written it will default to 1) of the item to get a custom item nested inside the appropriate namespace.
+}
+
+onEvent('item.registry', event => {
+    function createOrgan(organ) {
+        event.create(`${organ.modid}:${organ.itemID}`)
+        .group('chestcavity:organs')
+        .maxStackSize(organ.stackSize)
+        .displayName(organ.displayName)
+        .build()
+    }
+
+    //Create KubeJS Organs here
+    //Enter ID of the item and stacksize (default 1) of the item to get a custom item nested inside kubejscavity namespace. 
+    //Enter Full ID of the item to get a custom item nested inside the appropriate namespace.
+    //If a namespace is not supplied, it will default to kubejscavity.
     //This DOES NOT make these items into organs automatically, so go to server_scripts to add them to be organs!
-    //You also need to put the item textures under the appropriate namespace, defaulted into assets/kubejscavity/textures/item. change kubejscavity with the appropriate namespace.
+    //You also need to provide the item textures under the appropriate namespace, defaulted into assets/kubejscavity/textures/item. change kubejscavity with the appropriate namespace.
     
-    //thank you for A5TR0spud for allowing me to use his mod's spritework for these examplary organs! 
-    //check out his mod and its github: https://www.curseforge.com/minecraft/mc-mods/ccastroadds https://github.com/A5TR0spud/chest-cavity-astro-adds
-    
-    createOrgan('creative_soul')
-    createOrgan('actuator',16)
-    createOrgan('copper_wiring',4)
-    createOrgan('copper_frame',4)
-    createOrgan('phantom_pump')
-    createOrgan('filter_pump')
-    createOrgan('blood_pump')
-    createOrgan('jump_shaft',16)
-    createOrgan('golden_wiring',4)
-    createOrgan('clockwork_core')
-    createOrgan('gem_holster')
-    createOrgan('acid_mixer')
-    createOrgan('steel_actuator',16)
-    createOrgan('steel_cable',4)
-    createOrgan('steel_frame',4)
-    createOrgan('steel_detoxifier')
-    createOrgan('steel_core')
-    createOrgan('captured_soul')
-    createOrgan('iron_lung')
-    createOrgan('heart_of_gold')
-    createOrgan('hasty_muscle',16)
+    createOrgan(new KubeOrgan('creative_soul')
+        .build()
+    )
+
+    createOrgan(new KubeOrgan('actuator')
+        .stackSize(16)
+        .build()
+    )
+
+    createOrgan(new KubeOrgan('copper_wiring')
+        .stackSize(4)
+        .build()
+    )
+
+    createOrgan(new KubeOrgan('copper_frame')
+        .stackSize(4)
+        .build()
+    )
+
+    createOrgan(new KubeOrgan('phantom_pump').build())
+    createOrgan(new KubeOrgan('filter_pump').build())
+    createOrgan(new KubeOrgan('blood_pump').build())
+
+    createOrgan(new KubeOrgan('jump_shaft')
+        .stackSize(16)
+        .build()
+    )
+
+    createOrgan(new KubeOrgan('golden_wiring')
+        .stackSize(4)
+        .build()
+    )
+
+    createOrgan(new KubeOrgan('clockwork_core').build())
+    createOrgan(new KubeOrgan('gem_holster').build())
+    createOrgan(new KubeOrgan('acid_mixer').build())
+
+    createOrgan(new KubeOrgan('steel_actuator')
+        .stackSize(16)
+        .build()
+    )
+
+    createOrgan(new KubeOrgan('steel_cable')
+        .stackSize(4)
+        .build()
+    )
+
+    createOrgan(new KubeOrgan('steel_frame')
+        .stackSize(4)
+        .build()
+    )
+
+    createOrgan(new KubeOrgan('steel_detoxifier').build())
+    createOrgan(new KubeOrgan('steel_core').build())
+    createOrgan(new KubeOrgan('captured_soul').build())
+    createOrgan(new KubeOrgan('iron_lung').build())
+    createOrgan(new KubeOrgan('heart_of_gold').build())
+
+    createOrgan(new KubeOrgan('hasty_muscle')
+        .stackSize(16)
+        .build()
+    )
 })
+
+// Very special thanks to A5TR0spud for allowing us to use his mod's spritework for these examplary organs!
+// Check his mod out on CurseForge: https://www.curseforge.com/minecraft/mc-mods/ccastroadds
+// and Github: https://github.com/A5TR0spud/chest-cavity-astro-adds
